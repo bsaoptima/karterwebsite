326f8bdc31f63df991a06d41dd940db7797a4335
bsaoptima <bogdanstefan.agrici@gmail.com>
2023-07-21 18:38:22 +0100

    first commit

---
+++ karterwebsite/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <>
+      <div className="relative">p</div>
+    </>
+  )
+}
+
+export default App
