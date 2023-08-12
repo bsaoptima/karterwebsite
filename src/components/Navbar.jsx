8accf162e00b3803f7079b58077b5664a52d9fd1
bsaoptima <bogdanstefan.agrici@gmail.com>
2023-08-12 14:31:51 +0100

    fixed navbar on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png"
 
 
 export default function Navbar() {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsExpanded(prevState => !prevState);
+    };
+
     return (
-        
         <nav class="bg-[#F6F9FC] border-gray-200">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 py-6">
-            <a href="/" class="flex items-center">
-                <p className="text-xl font-alliance">&lt;karter&gt;</p>
-            </a>
-            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-[#F6F9FC]">
-                {/* <li>
-                <a href="/solutions" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#00A6B2] md:p-0">Solutions</a>
-                </li> */}
-                <li>
-                <a href="/contact" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#00A6B2] md:p-0">Contact</a>
-                </li>
-            </ul>
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 py-6">
+                <a href="/" class="flex items-center">
+                    <p className="text-xl font-alliance">&lt;karter&gt;</p>
+                </a>
+                <button
+                    onClick={toggleNavbar}
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                    aria-controls="navbar-default"
+                    aria-expanded={isExpanded}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div className={`w-full md:block md:w-auto ${isExpanded ? 'block' : 'hidden'}`} id="navbar-default">
+                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-[#F6F9FC]">
+                        {/* <li>
+                        <a href="/solutions" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#00A6B2] md:p-0">Solutions</a>
+                        </li> */}
+                        <li>
+                        <a href="/contact" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#00A6B2] md:p-0">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
         </nav>
 
     )
