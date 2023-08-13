52a0eaa0801cb4766ef6baa72cf3d67df6937f23
bsaoptima <bogdanstefan.agrici@gmail.com>
2023-08-13 21:12:55 +0100

    changes?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,7 +59,7 @@ function LandingSection({ scrollToContact, scrollToSolutions }) {
               </button>
             </div>
             <div className="flex flex-row items-center gap-x-2 md:gap-x-4 phone-hidden">
-              <div className="triangle"></div>
+              {/* <div className="triangle"></div> */}
               <a
                 onClick={scrollToSolutions}
                 className="text-sm md:text-lg font-alliance relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-black before:hover:scale-x-100 before:scale-x-0 before:origin-top-left before:transition before:ease-in-out before:duration-300"
