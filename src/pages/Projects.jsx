28b24e8d720e73fab6150f508d8c329a8a7716bf
bsaoptima <bogdanstefan.agrici@gmail.com>
2023-09-02 17:35:48 +0200

    fixed responsive

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -21,7 +21,7 @@ export default function ProjectsPage(){
                 </a>
                 
             </div>
-            <div className="grid grid-cols-3 gap-5 pb-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pb-10">
                 <article class="article-wrapper drop-shadow">
                     <div class="rounded-lg container-project">
                     </div>
